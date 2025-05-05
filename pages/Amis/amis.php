56a252decc6236b8fd7d1022a55c3782a3bfe6e6
corentinbondeau <corentin.bondeau@student.junia.com>
2025-05-05 12:43:17 +0200

    amis

--- pages/Amis/amis.php
+++ pages/Amis/amis.php
@@ -16,5 +16,30 @@
         <input type="submit" name="Envoyer" class="send_button" Value="Rechercher"/>
     </form>
 
+    <h2> Mes amis </h2>
+    <?php
+        if (isset($_SESSION['id'])) {
+            $id = $_SESSION['id'];
+            try {
+                require("connexion.php");
+                $reqPrep = "SELECT * FROM profil WHERE Id = :id";
+                $req = $conn->prepare($reqPrep);
+                $req->bindParam(':id', $id);
+                $req->execute();
+                $resultat = $req->fetch();
+            } catch (Exception $e) {
+                die("Erreur : " . $e->getMessage());
+            }
+
+            for ($index = 1; $index < 11; $index++) {
+                if ($resultat['Ami'.$index] != NULL) {
+                    echo "<p> Ami".$index." : " . $resultat['Ami'.$index] . "</p>";
+                }
+            }
+        } else {
+            echo "<p> Vous devez être connecté pour voir vos amis. </p>";
+        }
+    ?>
+
     </body>
 </html>
